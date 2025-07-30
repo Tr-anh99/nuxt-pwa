f87fb14d6a07c3a5234f836b7705a91155660323
trunganh <htranh2022@gmail.com>
2025-07-30 11:29:57 +0700

    init

---
+++ app/pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const pwa = usePWA()
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt Vite PWA</h1>
+    <ClientOnly>
+      PWA Installed: {{ pwa?.isPWAInstalled }}
+    </ClientOnly>
+    <NuxtLink to="/">
+      Home 1
+    </NuxtLink>
+  </div>
+</template>
